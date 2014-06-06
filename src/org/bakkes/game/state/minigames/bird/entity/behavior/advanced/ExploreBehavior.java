4a1cad183930d5d210f1834fac868e7f4f5966ca
U-chris-pc\Chris <chris.m@live.nl>
2014-06-06 04:02:26 +0200

    Added explore behavior

--- src/org/bakkes/game/state/minigames/bird/entity/behavior/advanced/ExploreBehavior.java
+++ src/org/bakkes/game/state/minigames/bird/entity/behavior/advanced/ExploreBehavior.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import org.bakkes.game.GameInfo;
 import org.bakkes.game.math.Vector2;
+import org.bakkes.game.math.pathfinding.heuristic.EuclideanDistance;
 import org.bakkes.game.math.pathfinding.heuristic.IHeuristicCalculator;
 import org.bakkes.game.math.pathfinding.heuristic.ManhattanHeuristic;
 import org.bakkes.game.state.minigames.bird.entity.MovingEntity;
@@ -11,16 +12,42 @@ import org.bakkes.game.state.minigames.bird.entity.behavior.IBehavior;
 import org.bakkes.game.state.minigames.bird.entity.behavior.simple.SeekBehavior;
 
 public class ExploreBehavior implements IBehavior {
-	private static final float HUNTING_DISTANCE = 30f; //hunt when within 30 pixels
+	private static final float HUNTING_DISTANCE = 130f; //hunt when within 30 pixels
 	private static final Random random = new Random(42);
 	private MovingEntity entity;
 	private SeekBehavior seek;
 	private Vector2 velocity;
-	
+	private Vector2 destination;
+	private int lastDest = 5;
+	private boolean destInverted = false;
+	private boolean currentX = true;
 	public ExploreBehavior(MovingEntity entity) {
 		this.entity = entity;
 		this.seek = new SeekBehavior(entity);
 		this.velocity = new Vector2(0f, 0f);
+		this.destination = entity.position.copy();
+		setNextDestination();
+	}
+	
+	private void setNextDestination() {
+		lastDest ++;
+		if(lastDest == 70) {
+			lastDest = 0;
+			destInverted = !destInverted;
+		}
+		if(lastDest == 35) {
+			currentX = !currentX;
+			
+		}
+		int xDiff = destInverted ? 5 : -5;
+		int yDiff = destInverted ? 5 : -5;
+		
+		if(currentX)
+			yDiff = 0;
+		else
+			xDiff = 0;
+		//this.destination = entity.position.copy();
+		this.destination.add(new Vector2(xDiff, yDiff));
 	}
 	
 	public Vector2 calculate(Vector2 target) {
@@ -30,20 +57,12 @@ public class ExploreBehavior implements IBehavior {
 		if(Math.abs(dist) < HUNTING_DISTANCE) {
 			return seek.calculate(target);
 		}
-		velocity.setX(velocity.getX() + random.nextFloat() * 4 - 2);
-		velocity.setY(velocity.getY() + random.nextFloat() * 4 - 2);
 		
-		Vector2 endPoint = velocity.copy();
-		endPoint.add(entity.position);
-		if(endPoint.getX() < 0 || endPoint.getX() > GameInfo.SCREEN_WIDTH) {
-			endPoint.setX(-endPoint.getX());
+		float distToDest = distCalc.getDistance(entity.position, destination);
+		if(Math.abs(distToDest) < 2) {
+			setNextDestination();
 		}
-		
-		if(endPoint.getY() < 0 || endPoint.getY() > GameInfo.SCREEN_HEIGHT) {
-			endPoint.setY(-endPoint.getY());
-		}
-		
-		return velocity;
+		return seek.calculate(destination);
 	}
 	
 	@Override
