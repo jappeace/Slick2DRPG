4e8de9113d28e78b9ff5d828fb65c6cd03bb1502
Jappie Klooster <jappieklooster@hotmail.com>
2015-02-01 19:42:14 +0800

    deleting the pokmeon files for now, finshed the ripping script

--- src/main/python/screenscraper.py
+++ src/main/python/screenscraper.py
@@ -1,15 +1,68 @@
-from requests import get, codes, post, exceptions, Session
-from json import loads
+from requests import Session
 from bs4 import BeautifulSoup
+from shutil import copyfileobj
+from os import makedirs
+from errno import EEXIST
 
 properDir = '../../../build/'
+baseUrl = 'http://bulbapedia.bulbagarden.net/'
+wikiUrl = baseUrl+ 'wiki/'
+
 def tohtml(lines):
     f = open(properDir + "output.html", "w", encoding='utf8')
     f.writelines(lines)
     f.close()
 
+session = Session()
+def getImage(imgName, folder, url):
+
+    response = session.get(url, stream=True)
+    if response.status_code != 200:
+        print("error, invalid status code")
+        return
+
+    path = "../resources/sprites/pokemon/"+ folder.lower()+"/"
+    try:
+        makedirs(path)
+    except OSError as exception:
+        if exception.errno != EEXIST:
+            raise
+
+    with open(
+        path + imgName.lower()+".png",
+        'wb'
+    ) as f:
+        response.raw.decode_content = True
+        copyfileobj(response.raw, f)
+
+# the stupid wikipedia system has a html page between
+# the real image and it stores it semi randomly, so we
+# have to scrape that
+def requestImageUrl(fileName):
+    return BeautifulSoup(
+        session.get(
+            wikiUrl+"File:"+fileName+".png"
+        ).text
+    ).img.get('src')
+
+class Pokemon:
+    name = ""
+    id = ""
+
+def handleIteration(i):
+    nextPage = i.a.get('href')
+    pokemon = Pokemon()
+    pokemon.name = i.find_all('td')[2].a.text
+    pokemon.id = i.find_all('td')[0].text.strip()
+
+    getImage("small", pokemon.name, i.img.get('src'))
+    getImage("front", pokemon.name, requestImageUrl("Spr_4d_"+pokemon.id))
+    getImage("back", pokemon.name, requestImageUrl("Spr_b_4d_"+pokemon.id))
+    getImage("shiny_front", pokemon.name, requestImageUrl("Spr_4d_"+pokemon.id+"_s"))
+    getImage("shiny_back", pokemon.name, requestImageUrl("Spr_b_4d_"+pokemon.id+"_s"))
+    getImage("big", pokemon.name, requestImageUrl(pokemon.id+pokemon.name))
+
 #target = 'http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_base_stats_(Generation_II-V)'
-#session = Session()
 
 #listTable = BeautifulSoup( session.get(target).text.encode('utf8')).find('table').find_all('tr')
 # using a file is a speedup in testing but needs to be replaced in final
@@ -24,6 +77,5 @@ listTable = BeautifulSoup(page).find('table').find_all('tr')
 
 childsIter = iter(listTable)
 next(childsIter)
-for child in childsIter:
-    print(child.encode('utf8'))
+handleIteration(next(childsIter))
 
---
+++ src/main/resources/sprites/pokemon/README.md
@@ -0,0 +1,25 @@
+
+# Pokemon sprites
+these are all the pokemon sprites, ripped from [bublapedia](
+http://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pok%C3%A9mon)
+)
+
+the script that did the ripping can be found under
+`src/main/python/*`
+The excact file will probably change,
+but I don't intend on making many python scripts.
+
+## order scheme
+the sprites are put into the folder with the name of the proper pokemon.
+there is no clever reason for this except it makes sprites grouped better.
+My initial idea was to put all sprites based on big small back front
+folders and then the pokemon name as image.
+But I think this is a anoying division, better too group all data of a specific
+pokemon in the same folder
+
+### image explenation
+the back and front images are used in battle
+the small image is used to show the pokemon in menus
+
+the big one is used when the pokemon is selected
+
