2c14d9dd39262383e8d38d58c7f1d2c3a784c9fd
Jappie Klooster <jappieklooster@hotmail.com>
2015-02-02 00:28:05 +0800

    fixed several bugs, for example the handeling of the anoying nidoran character and added a caching mechanism that speeds up the reebooting of the script, if a connection now fails the current directory is removed, this way all existing files are sortof gaurenteed to be complete

--- src/main/python/screenscraper.py
+++ src/main/python/screenscraper.py
@@ -1,7 +1,7 @@
 from requests import Session, exceptions
 from bs4 import BeautifulSoup
-from shutil import copyfileobj
-from os import makedirs
+from shutil import copyfileobj, rmtree
+import os
 from errno import EEXIST
 from hashlib import md5
 import socket
@@ -10,37 +10,46 @@ properDir = '../../../build/'
 baseUrl = 'http://bulbapedia.bulbagarden.net/'
 wikiUrl = baseUrl+ 'wiki/'
 timeout = 15
+storePath = "../resources/sprites/pokemon/"
 def tohtml(lines):
     f = open(properDir + "output.html", "w", encoding='utf8')
     f.writelines(lines)
     f.close()
 
 session = Session()
+def fileIsGood(fileName):
+    with open(fileName, 'rb') as f:
+        if md5(f.read()).hexdigest() == '6e78c36b63bbb23649a8b8582ae706b0':
+            print("file is not good, (got a known error file)")
+            return False
+    return True
 def fetchImage(imgName, folder, url):
+
+    path = storePath + folder.lower()+"/"
+    fileName = path + imgName.lower()+".png"
+    if os.path.isfile(fileName) and fileIsGood(fileName):
+        print(fileName + " already exists, skipping")
+        return True
 
     response = session.get(url, stream=True, timeout=timeout)
     if response.status_code != 200:
         print("error, invalid status code")
         return
 
-    path = "../resources/sprites/pokemon/"+ folder.lower()+"/"
     try:
-        makedirs(path)
+        os.makedirs(path)
     except OSError as exception:
         if exception.errno != EEXIST:
             raise
 
-    fileName = path + imgName.lower()+".png"
     with open(
         fileName,
         'wb'
     ) as f:
         response.raw.decode_content = True
         copyfileobj(response.raw, f)
-    with open(fileName, 'rb') as f:
-        if md5(f.read()).hexdigest() == '6e78c36b63bbb23649a8b8582ae706b0':
-            print("file is not good")
-            return False
+    if not fileIsGood(fileName):
+        return False
     return True
 
 # the stupid wikipedia system has a html page between
@@ -60,28 +69,43 @@ def fetchSprite(imgName, folder, url, afterurl = ""):
     fetchImage(imgName, folder, requestImageUrl(url+"_m" + afterurl))
     fetchImage(imgName+"_female", folder, requestImageUrl(url+"_f" + afterurl))
 
+def getName(i):
+    return i.find_all('td')[2].a.text.replace("\u2640","").replace("\u2642","t")
+
 def handleIteration(i):
     nextPage = i.a.get('href')
 
-    name = i.find_all('td')[2].a.text
+    name =getName(i)
     id = i.find_all('td')[0].text.strip()
 
-    print("handeling " + name)
+    try:
+        print("handeling " + name)
+    except UnicodeEncodeError as err:
+        print(("handeling " + name).encode("utf8"))
+
     fetchImage("small", name, i.img.get('src'))
     fetchSprite("front", name, "Spr_4d_"+id)
     fetchSprite("back", name, "Spr_b_4d_"+id)
     fetchSprite("shiny_front", name, "Spr_4d_"+id,"_s")
     fetchSprite("shiny_back", name, "Spr_b_4d_"+id,"_s")
     fetchImage("big", name, requestImageUrl(id+name))
+
+def resetCurrentAndRetry(i):
+        # clear the working tree so the existing file check is cirumvented,
+        # if a file is half loaded it will be now fully loaded
+        path = storePath+getName(i).lower()
+        if os.path.isdir(path):
+            rmtree(path)
+        handleIterationIgnoringConnectionBullshit(i)
 def handleIterationIgnoringConnectionBullshit(i):
     try:
         handleIteration(i)
     except exceptions.ConnectionError as connectionError:
         print("got a connection error, trying again")
-        handleIterationIgnoringConnectionBullshit(i)
+        resetCurrentAndRetry(i)
     except socket.timeout as time:
         print("had a timeout")
-        handleIterationIgnoringConnectionBullshit(i)
+        resetCurrentAndRetry(i)
 
 target = wikiUrl+'List_of_Pok%C3%A9mon_by_base_stats_(Generation_II-V)'
 listTable = BeautifulSoup( session.get(
